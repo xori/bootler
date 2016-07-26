2ad012a8060d2a44e61f9ed83bdef350d0577ca1
Evan Verworn <evan.verworn@dsbn.org>
2016-07-26 16:52:47 -0400

    File structure defined

---
+++ test/test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+describe('Bot Engine', function() {
+  it('.onMention', function() { });
+  it('.on', function() { });
+  it('.brain');
+  it('.test');
+  it('starts without errors');
+
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert(-1);
+    });
+  });
+});
