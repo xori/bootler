5c06194a49bc57da6b1c65e55da9b35d6037f0f8
Evan Verworn <xori@users.noreply.github.com>
2016-07-27 11:45:52 -0400

    Proper dice support added. (#1)
    * Created test framework
    * Created tests
    * remove fs-extra package

--- plugins/dice.js
+++ plugins/dice.js
@@ -1,15 +1,34 @@
+const R = require('roll');
 
 module.exports = function(engine) {
-  engine.on(/^(\d+)d(\d+)$/i, function(message, params, send) {
-    let total = 0;
-    let script = "";
-    let n = parseInt(params[1]);
-    let die = parseInt(params[2]);
-    for(let i = 0; i < n; i++) {
-      let amount = Math.floor(Math.random() * die) + 1;
-      total += amount;
-      script += `(${amount})`;
+  engine.respond(/roll ([^\s]+)$/i, function(message, params, send) {
+    let ask = params[1];
+    let dice = new R();
+
+    if(dice.validate(ask)) {
+      send(`${ask} => ${dice.roll(ask).result}`);
+    } else {
+      send(`That ain't a valid roll.`)
     }
-    send(`${script} => ${total}`)
-  })
+  });
+}
+
+var assert = require('assert');
+module.exports.test = function(engine) {
+  describe('Dice Plugin', function() {
+
+    it('should do simple evaluations', function(done) {
+      engine.test('@bot roll 3d16', function(text) {
+        assert( /3d16 => \d+/.test(text) );
+        done();
+      });
+    });
+
+    it('should respond appropriately to incorrect syntax', function(done) {
+      engine.test('@bot roll 3$16', function(text) {
+        assert.equal(text, "That ain't a valid roll.");
+        done();
+      });
+    })
+  });
 }
--- src/Server.js
+++ src/Server.js
@@ -1,44 +1,20 @@
-var config = {};
+let config = {};
 if(!process.env.DISCORD_TOKEN)
   config = require('../config');
-var pack = require('../package.json');
-var Discord = require("discord.js");
-var bot = new Discord.Client();
+let pack = require('../package.json');
+let Discord = require("discord.js");
+let Engine = require('./Engine');
 
-var plugins = [  ];
-var engine = {
-  on: function(regex, callback) {
-    plugins.push({regex, callback});
-  },
-  onMention: function(regex, callback) {
-    plugins.push({regex, callback, mention: true});
-  }
-}
-
-require('../plugins/dice')(engine)
+let bot = new Discord.Client();
+let engine = new Engine();
 
 bot.on("message", function(message) {
-    if(message.content === "ping") {
-      bot.reply(message, `pong ${pack.version}`);
-    } else {
-      let mentioned = message.mentions.length > 0 && message.mentions[0].bot;
-      let content = message.cleanContent;
-      for(let i = 0; i < plugins.length; i++) {
-        if(!mentioned && plugins[i].mention) continue;
-        let capture = content.match(plugins[i].regex)
-        if(capture) {
-          plugins[i].callback(message, capture, function(m) {
-            bot.reply(message, m);
-          })
-        }
-      }
-    }
+  engine.handle(message, bot);
 });
 
 bot.loginWithToken(process.env.DISCORD_TOKEN || config.token)
-.then(function() {
-  console.log("Connected.");
-})
-.catch(function(err) {
-  console.error(err);
+  .then(function() {
+    console.log("Connected.");
+  }).catch(function(err) {
+    console.error(err);
 });
