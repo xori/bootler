2f83ec1e05e41b667eb27c5f557b9eb0b12c75cc
Evan Verworn <evan.verworn@dsbn.org>
2016-08-02 08:14:24 -0400

    idea

---
+++ plugins/character.js
@@ -0,0 +1,18 @@
+/***
+  Character Rolls
+    myself  > @Bootler character:set https://sheets.google.com/1234
+    bootler > Sir James McFillion, lvl 12 ready.
+    myself  > I ready myself, then spring around the corner casting 𝔐𝔞𝔤𝔦𝔠 𝔐𝔦𝔰𝔰𝔩𝔢
+              on any Goblin I see.
+    bootler > 𝔐𝔞𝔤𝔦𝔠 𝔐𝔦𝔰𝔰𝔩𝔢 (Hit: 18, Damage: 18)
+    goblin  > I pull my :knife: and attack.
+    bootler > :knife: (Hit: 12, Damage: 5)
+
+  This module would allow for grabbing stats and attacks from a google sheet,
+  then when a given keyword perform a roll from the sheet in the form of
+  +-------------+---------+--------+
+  | 𝔐𝔞𝔤𝔦𝔠 𝔐𝔦𝔰𝔰𝔩𝔢 | d20+CHA | 3d6+3  |
+  +-------------+---------+--------+
+  | Firebolt    | d20+WIS | 2d10+2 |
+  +-------------+---------+--------+
+***/
