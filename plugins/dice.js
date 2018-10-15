11165f0a73362fc50fc882f95baa95424e0461d1
RexHamilton <RexHamilton@users.noreply.github.com>
2018-10-15 18:59:36 -0400

    Make dice better (#11)
    * listens for messages starting with "roll"
    * listens for messages starting with <n>d<n>

--- plugins/dice.js
+++ plugins/dice.js
@@ -1,7 +1,15 @@
 const R = require('roll');
 
 module.exports = function(engine) {
-  engine.on(/roll (.+)$/i, function(message, params, send) {
+  engine.on(/^roll (.+)$/i, roll);
+  
+  engine.on(/(^\d+d\d+(?:[\+\-\*\/]\d+)*)/i, roll);
+}
+
+function roll(message, params, send) {
+	if (message.author.bot){ // Don't get into an infinite loop with yourself
+		return;
+	}
     let ask = params[1].split(" ");
     let dice = new R();
 
@@ -12,13 +20,12 @@ module.exports = function(engine) {
       if(dice.validate(_)) {
         result += _ + " ";
         let theroll = dice.roll(_)
-        resultValue = `${theroll.result} [${theroll.rolled.join(', ')}]`;
+        resultValue += `${theroll.result} [${theroll.rolled.join(', ')}] `;
       } else {
         return send(`${_} isn't in standard dice format.`)
       }
     }
     send(result += `=> ${resultValue}`);
-  });
 }
 
 var assert = require('assert');
@@ -29,21 +36,35 @@ module.exports.test = function(engine) {
       engine.test('roll 3d16', function(text) {
         assert( /3d16 => \d+/.test(text) );
         done();
-      });
+      }, {author:{bot:false}});
     });
 
     it('should do complex evaluations', function(done) {
       engine.test('roll 3d16 4d20*2+5', function(text) {
         assert(/3d16 4d20\*2\+5 => \d+/.test(text), text);
         done();
-      });
+      }, {author:{bot:false}});
     });
 
     it('should respond appropriately to incorrect syntax', function(done) {
       engine.test('roll 3$16', function(text) {
         assert.equal(text, "3$16 isn't in standard dice format.");
         done();
+      }, {author:{bot:false}});
+    });
+
+    it('should respond if just a dice roll', function(done) {
+      engine.test('5d10*2+5', function(text) {
+        assert(/5d10\*2\+5 => \d+/.test(text), text);
+        done();
+      }, {author:{bot:false}});
+    });
+	
+    it('should not respond to any text with roll', function(done) {
+      engine.test('Hey, don\'t roll anything here', function(text) {
+        fail();
       });
-    })
+	  setTimeout(done, 100);
+    }, {author:{bot:false}});
   });
 }
